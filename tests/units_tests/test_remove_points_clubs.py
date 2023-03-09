396ec42912ca969323be520824a1f14a2f0976e8
Antoine Cardon <c.antoine97@gmail.com>
2023-03-09 21:21:00 +0100

    test remove points

---
+++ tests/units_tests/test_remove_points_clubs.py
@@ -0,0 +1,20 @@
+""" TEST BOOKING COMPETITION : Check clubs points """
+
+
+def test_to_remove_points_clubs():
+    server.clubs[0]["points"] = "5"
+
+    points_of_club_before_booking = server.clubs[0]["points"]
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "places": 3,
+            "club": server.clubs[0]["name"],
+            "competition": server.competitions[0]["name"],
+        },
+    )
+
+    points_of_club_after_booking = server.clubs[0]["points"]
+
+    assert int(points_of_club_after_booking) < int(points_of_club_before_booking)
+    assert response.status_code == 200
